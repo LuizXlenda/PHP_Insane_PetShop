713f74cd7487e9057461f70d4720a8ea35353435
LuizXlenda <riquestorrer@gmail.com>
2025-04-25 10:55:11 -0300

    Rascunho

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Pet Shop</title>
+</head>
+<body>
+    <header>
+        <div>
+        <nav><!--Representa uma seção com links de navegação-->
+            <ul><!--Representa uma lista de intens sem ordenação numerica-->
+                <a href="./index.html"><li>Home</li></a><!--Faz uma referencia a uma URL externa-->
+                <a href="./tabela.html"><li>Tabela</li></a><!--Representa um item que faz parte de uma lista e está contido em <ul>-->
+                <a href="./tabela.html"><li>Tabela</li>
+                <a href="./tabela.html"><li>Tabela</li>
+                <a href="./contato.html"><li>Contato</li></a>    
+            </ul>
+        </nav>
+        </div>
+    </header>
+
+    <br><br>
+
+    <main><!--Esta tag define o conteúdo principal dentro do <body> em sua aplicação-->
+        <section id="principal">
+            <h1>Bem-vindo ao Pet Shop!</h1>
+            <p>O melhor para seu pet!</p>
+        </section>
+    </main>
+
+
+
+
+
+    <footer>
+        <small>&copy; Copyright 2025 Todos os direitos reservados.</small>
+    </footer>
+    
+</body>
+</html>
